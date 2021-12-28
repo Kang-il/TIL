4d07467a74fcd1489d6493470ea3efc6d9eb3ed8
Kang-il <h5tchi@naver.com>
2021-12-28 23:51:24 +0900

    [백준] 1248 맞춰봐 C++

---
+++ brute_force/bf_ex31.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int N;
+int S[10][10];
+int A[10];
+
+bool ok(int idx){
+  int sum=0;
+  for(int i=idx;i>=0;i--){
+      sum+=A[i];
+      if(S[i][idx]==0 && sum!=0) return false;
+      if(S[i][idx]>0 && sum<=0) return false;
+      if(S[i][idx]<0 && sum>=0) return false;
+  }
+  return true;
+}
+
+bool go(int idx){
+    if(idx==N) return true;
+    if(S[idx][idx]==0){
+        A[idx]=0;
+        return ok(idx) && go(idx+1);
+    }
+    for(int i=1;i<=10;i++){
+        A[idx]=S[idx][idx]*i;
+        if(ok(idx)&&go(idx+1)) return true;
+    }
+    return false;
+}
+int main(){
+    //(1248)
+    // 맞춰봐
+    // -10 부터 10까지 N개의 정수로 이루어진 수열이 있다 (N<=10)
+    // S[i][j]=A[i]+A[i+1]+...+A[j]가 0보다 크면 +, 0보다 작으면 -, 0이면 0
+    // S가 주어졌을 때 가능한 A를 아무거나 찾는 문제
+    // i는 j보다 항상 작거나 같다.
+    
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    std::cin>>N;
+    std::string s;
+    std::cin>>s;
+    int cnt=0;
+   
+    go(0);
+    for(int i=0;i<N;i++){
+        std::cout<<A[i]<<' ';
+    }
+    std::cout<<'\n';
+    return 0;
+}
